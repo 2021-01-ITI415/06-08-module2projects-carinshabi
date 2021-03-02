a7b6bde36d68b0e1e4a9092710c13d9235d72b67
Carin Shabi <crs255@scarletmail.rutgers.edu>
2021-03-01 22:20:41 -0500

    Work Session 2

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -1,28 +1,25 @@
-﻿using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // This will be used later in the project
+using UnityEngine.UI; // This will be used later in the project
 
 
 public class Prospector : MonoBehaviour {
-
 	static public Prospector 	S;
 
 	[Header("Set in Inspector")]
 	public TextAsset			deckXML;
 
-
 	[Header("Set Dynamically")]
 	public Deck					deck;
 
 	void Awake(){
-		S = this;
+		S = this; // Set up a Singleton for Prospector
 	}
 
 	void Start() {
-		deck = GetComponent<Deck> ();
-		deck.InitDeck (deckXML.text);
+		deck = GetComponent<Deck> (); // Get the Deck
+		deck.InitDeck (deckXML.text); // Pass DeckXML to it
 	}
-
 }
