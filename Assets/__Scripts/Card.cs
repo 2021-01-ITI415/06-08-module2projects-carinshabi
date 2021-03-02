ec66ac1d2b9a361bce742f5518d218d3b9865b05
Carin Shabi <crs255@scarletmail.rutgers.edu>
2021-03-02 17:19:17 -0500

    Work Session 6

--- Assets/__Scripts/Card.cs
+++ Assets/__Scripts/Card.cs
@@ -1,8 +1,9 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
 public class Card : MonoBehaviour {
+<<<<<<< HEAD
 	[Header("Set Dynamically")]
 	public string suit; // Suit of the Card (C,D,H, or S)
 	public int rank; // Rank of the Card (1-14)
@@ -24,20 +25,55 @@ public class Card : MonoBehaviour {
 		}
 	}
 }
+=======
+>>>>>>> parent of a7b6bde (Work Session 2)
 
-[System.Serializable] // A Serializable class is able to be edited in the Inspector 
-public class Decorator {
-	// This class stores information about each decorator or pip from DeckXML
-	public string type; // For card pips, type = 'pip"
-	public Vector3 loc; // For location of the Sprite on the Card
-	public bool flip = false; // Whether to flip the Sprite vertically
-	public float scale = 1.0f; // The scale of the Sprite
+	public string    suit;
+	public int       rank;
+	public Color     color = Color.black;
+	public string    colS = "Black";  // or "Red"
+	
+	public List<GameObject> decoGOs = new List<GameObject>();
+	public List<GameObject> pipGOs = new List<GameObject>();
+	
+	public GameObject back;  // back of card;
+	public CardDefinition def;  // from DeckXML.xml		
+
+
+	public bool faceUp {
+		get {
+			return (!back.activeSelf);
+		}
+
+		set {
+			back.SetActive(!value);
+		}
+	}
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+} // class Card
+
+[System.Serializable]
+public class Decorator{
+	public string	type;			// For card pips, tyhpe = "pip"
+	public Vector3	loc;			// location of sprite on the card
+	public bool		flip = false;	//whether to flip vertically
+	public float 	scale = 1.0f;
 }
 
 [System.Serializable]
-public class CardDefinition {
-	// This class stores information for aech rank of card
-	public string face; // Sprite to use for face cart
-	public int rank;    // The rank (1-13) of this card, Value from 1-13 (Ace-King)
-	public List<Decorator> pips = new List<Decorator>();  // Pips Used
+public class CardDefinition{
+	public string	face;	//sprite to use for face cart
+	public int		rank;	// value from 1-13 (Ace-King)
+	public List<Decorator>	
+					pips = new List<Decorator>();  // Pips Used
 }
