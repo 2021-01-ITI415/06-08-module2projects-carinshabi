afa610442cc93b054ca31c73e023b1f9e7009178
Carin Shabi <crs255@scarletmail.rutgers.edu>
2021-03-04 22:03:17 -0500

    Work Session 6

--- Assets/__Scripts/Card.cs
+++ Assets/__Scripts/Card.cs
@@ -14,15 +14,77 @@ public class Card : MonoBehaviour {
 
 	public GameObject back;  // back of card;
 	public CardDefinition def;  // from DeckXML.xml	
-	
-	public bool faceUp { 
+
+	// List of the SpriteRenderer Components of this GameObject and its children
+	public SpriteRenderer[] spriteRenderers;
+
+    void Start() {
+		SetSortOrder(0); // Ensures that the card starts properly depth sorted
+    }
+
+	// If spriteRenderers is not yet defined, this function defines it
+	public void PopulateSpriteRenderers() { 
+		// If spriteRenderers is null or empty 
+		if (spriteRenderers == null || spriteRenderers.Length == 0) {
+			// Get SpriteRenderer Components of this GameObject and its children
+			spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+		}
+	}
+
+	// Sets the sortingLayerName on all SpriteRenderer Components 
+	public void SetSortingLayerName(string tSLN) {
+		PopulateSpriteRenderers();
+
+		foreach (SpriteRenderer tSR in spriteRenderers) {
+			tSR.sortingLayerName = tSLN;
+		}
+	}
+
+	// Sets the sortingOrder of all SpriteRenderer Components 
+	public void SetSortOrder(int sOrd) {
+		PopulateSpriteRenderers();
+
+		// Iterate through all the spriteRenderers as tSR
+		foreach (SpriteRenderer tSR in spriteRenderers) {
+			if (tSR.gameObject == this.gameObject) {
+				// If the gameObject is this.gameObject, it's the background
+				tSR.sortingOrder = sOrd; // Set it's order to sOrd
+				continue; // And continue to the next iternation of the loop
+
+			}
+
+			// Each of the children of this GameObject are named 
+			// switch based on the names
+			switch (tSR.gameObject.name) {
+				case "back": // if the name is "back"
+					// Set it to the highest layer to cover the other sprites
+					tSR.sortingOrder = sOrd + 2;
+					break;
+
+				case "face": // if the name is "face"
+				default: // or it it's anything else
+					// Set it to the middle layer to be avoce the background
+					tSR.sortingOrder = sOrd + 1;
+					break;
+			}
+		}
+	}
+
+    public bool faceUp { 
 		get {
 			return (!back.activeSelf);
 		}
 		set {
 			back.SetActive(!value);
 		}
+
 	}
+
+	// Virtual methods can be overrideen by subclass methods with the same name
+	virtual public void OnMouseUpAsButton() {
+		print(name); // When clicked, this outputs the card name
+	}
+	
 } // class Card
 
 
@@ -36,7 +98,7 @@ public class Decorator{
 }
 
 [System.Serializable]
-public class CardDefinition{
+public class CardDefinition {
 	public string	face;	//sprite to use for face cart
 	public int		rank;	// value from 1-13 (Ace-King)
 	public List<Decorator>	
